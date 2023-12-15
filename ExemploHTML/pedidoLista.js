91cf063a4a14735c35290afc49f6dcd1f4455003
Diogo Fernandes <diogoafernandes20@gmail.com>
2023-12-15 12:05:25 +0000

    Página para ver os pedidos como cliente ou profissional.

--- ExemploHTML/pedidoLista.js
+++ ExemploHTML/pedidoLista.js
@@ -1,20 +1,48 @@
-window.OnPageLoad = function () {}
+const params = new URLSearchParams(window.location.search);
+const tipoUtilizador = params.get('usr');
+
+var colunas;
+var dados;
+
+window.OnPageLoad = function () { }
 window.onload = function () {
+    if (tipoUtilizador == 1) {
+        document.getElementById('titulo').innerHTML = "Histórico de Pedidos";
+    }
+    else if (tipoUtilizador == 2) {
+        document.getElementById('titulo').innerHTML = "Pedidos Disponíveis";
+    }
+    else {
+        document.getElementById('titulo').innerHTML = "Pedidos";
+    }
 
+    CarregaDados();
+    CriarTabela();
+}
 
-    var tabela = document.getElementById('tabela');
-    var tabelaHead = tabela.createTHead().insertRow(0);
-    tabela.createTBody().insertRow(0);
-    var tabelaBody = tabela.getElementsByTagName('tbody')[0];
-
-
-    var colunas = [
+function CarregaDados() {
+    colunas = [
         { nome: 'Código', classe: 'col-12 col-md-1' },
         { nome: 'Tipo', classe: 'col-12 col-md-2' },
         { nome: 'Disponibilidade', classe: 'col-12 col-md-4' },
         { nome: 'Valor', classe: 'col-12 col-md-2' },
         { nome: 'Opções', classe: 'col-12 col-md-1' }
     ];
+
+    dados = [
+        { id: 1, cliente: 1, tipo: 'Tipo 1', disponibilidade: '2024-01-01 -> 2024-02-01', valor: 44.50, historico: false },
+        { id: 2, cliente: 2, tipo: 'Tipo 3', disponibilidade: '2024-01-02 -> 2024-06-01', valor: 244.50, historico: true },
+        { id: 3, cliente: 1, tipo: 'Tipo 4', disponibilidade: '2024-01-15 -> 2024-02-20', valor: 70.00, historico: false }
+    ];
+
+}
+
+function CriarTabela() {
+    var tabela = document.getElementById('tabela');
+    var tabelaHead = tabela.createTHead().insertRow(0);
+    tabela.createTBody().insertRow(0);
+    var tabelaBody = tabela.getElementsByTagName('tbody')[0];
+
 
 
     colunas.forEach(tituloColuna => {
@@ -25,13 +53,11 @@ window.onload = function () {
         tabelaHead.appendChild(th);
     });
 
-    var dados = [
-        { id: 1, cliente: 1, tipo: 'Tipo 1', disponibilidade: '2024-01-01 -> 2024-02-01', valor: 44.50 },
-        { id: 2, cliente: 2, tipo: 'Tipo 3', disponibilidade: '2024-01-02 -> 2024-06-01', valor: 244.50 },
-        { id: 3, cliente: 1, tipo: 'Tipo 4', disponibilidade: '2024-01-15 -> 2024-02-20', valor: 70.00 }
-    ];
-
     dados.forEach(item => {
+        if (tipoUtilizador == 2 && item.historico) {
+            return;
+        }
+
         var linha = tabelaBody.insertRow(tabelaBody.rows.length);
 
         var c1 = linha.insertCell(0);
@@ -41,9 +67,14 @@ window.onload = function () {
         var c5 = linha.insertCell(4);
 
         // Botões para demonstração, se for cliente não pode fazer proposta, se for profissional não pode editar pedido.
-        var botoes = '<a title="Ver Pedido" class="pointer" onClick="verPedido(' + item.id + ');"><span class="material-icons">drafts</span></a>' + 
-            '<a title="Fazer Proposta" class="pointer" onClick="fazerProposta(' + item.id + ');"><span class="material-icons">send</span></a>' + 
-            '<a title="Editar Pedido" class="pointer" onClick="editarPedido(' + item.id + ');"><span class="material-icons">create</span></a>';
+        var botoes = '<a title="Ver Pedido" class="pointer" onClick="verPedido(' + item.id + ');"><span class="material-icons">drafts</span></a>';
+
+        if (tipoUtilizador == 1 && !item.historico) {
+            botoes += '<a title="Editar Pedido" class="pointer" onClick="editarPedido(' + item.id + ');"><span class="material-icons">create</span></a>';
+        }
+        if (tipoUtilizador == 2 && !item.historico) {
+            botoes += '<a title="Fazer Proposta" class="pointer" onClick="fazerProposta(' + item.id + ');"><span class="material-icons">send</span></a>';
+        }           
 
         c1.innerHTML = item.id;
         c2.innerHTML = item.tipo;
@@ -51,5 +82,4 @@ window.onload = function () {
         c4.innerHTML = item.valor;
         c5.innerHTML = botoes;
     });
-
 }
